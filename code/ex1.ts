db79645bcc7820918e14b6e01763541158b30bc4
Maiara Ávila <maiaroviski@gmail.com>
2025-11-27 18:55:58 -0300

    pi

---
+++ code/ex1.ts
@@ -0,0 +1,4 @@
+let pi = 3.14159;
+let tau = pi * 2;
+
+console.log(`${tau} is ${pi} times two.`)
---
+++ code/ex5.ts
@@ -0,0 +1,7 @@
+
+const sequence: number[] = Array.from(Array(10).keys());
+const animals: string [] = ['pangolin', 'aardvark', 'echdna', 'binturong'];
+const stringAndNumbers : (number | string)[] = [1, 'one', 2, 'two', 3, 'three'];
+const allMyArrays : (number | string)[][]  = [sequence, animals, stringAndNumbers];
+
+console.log(allMyArrays);
